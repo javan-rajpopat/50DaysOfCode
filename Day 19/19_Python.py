babb16a45a8364e10e744dbd7684103daa96ac7d
Javan Rajpopat <43393611+javan-rajpopat@users.noreply.github.com>
2019-01-19 22:56:22 -0500

    Add files via upload

---
+++ Day 19/19_Python.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        output=pow(x,n)
+        return output
+        
